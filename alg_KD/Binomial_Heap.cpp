ff12043dd4f6d0e1fe7cd8feef4bd1ad1796d352
RafMat159 <85128542+RafMat159@users.noreply.github.com>
2021-12-08 20:32:17 +0100

    Add files via upload

--- alg_KD/Binomial_Heap.cpp
+++ alg_KD/Binomial_Heap.cpp
@@ -205,7 +205,7 @@ Node* binomial_heap_minimum(Binomial_Heap* H)
 	Node* root = H->getRoot();
 	int min = std::numeric_limits<int>::max();
 	while (root != nullptr) {
-		if (root->getValue() <= min) {
+		if (root->getValue() < min) {
 			min = root->getValue();
 			min_ptr = root;
 		}
@@ -234,50 +234,61 @@ void binomial_heap_decrease_key(Binomial_Heap* H, Node* ptr, int value)
 Node* binomial_heap_extract_min(Binomial_Heap*& H) 
 {
 	Node* min = binomial_heap_minimum(H);
-	Node* tmp = H->getRoot();	//zmienna pozwalalaj¹ca na poruszanie siê po liœcie korzeni kopca H; wskaŸnik do powsta³ej listy korzeni kopca H`
-	Node* minCh = min->getChild(); //wskaŸnik do poruszania siê po liœcie synów minimalnego korzenia
-	min->setChild(nullptr); //zerwanie po³aczenia minimalnego wêz³a z jego synami
-	std::vector<Node*> tabN; //wektor wskaŸników na wêz³y bêd¹ce dzieæmi minimalnego korzenia 
+	Node* tmp = H->getRoot();									//zmienna pozwalalaj¹ca na poruszanie siê po liœcie korzeni kopca H; wskaŸnik do powsta³ej listy korzeni kopca H`
+	Node* minCh = min->getChild();								//wskaŸnik do poruszania siê po liœcie synów minimalnego korzenia
+	Node* secondRoot = H->getRoot()->getSibling();				//drugi korzeñ na liœcie, potrzebny jeœli usuwan¹ wartoœci¹ jest pierwszy korzeñ
+	min->setChild(nullptr);										//zerwanie po³aczenia minimalnego wêz³a z jego synami
+	std::vector<Node*> tabN;									//wektor wskaŸników na wêz³y bêd¹ce dzieæmi minimalnego korzenia 
 
 	while (tmp->getSibling() != min && tmp != min) {	
 		tmp = tmp->getSibling();
 	}
-	if (tmp->getSibling()->getSibling() == nullptr)
-		tmp->setSibling(nullptr);	//usuniêcie korzenia o minimalnej wartoœci wêz³a z listy korzeni (wskaŸnik brata ustawiany jest na nullptr, poniewa¿ usuwany korzeñ jest ostatnim na liœcie korzeni)
-	else
-		tmp->setSibling(tmp->getSibling()->getSibling()); //usuniêcie korzenia o minimalnej wartoœci wêz³a z listy korzeni( sytuacja, w której wêze³ usuwany nie jest ostatnim korzeniem na liœcie korzeni)
+	if (tmp != min) {
+		if (tmp->getSibling()->getSibling() == nullptr)
+			tmp->setSibling(nullptr);	//usuniêcie korzenia o minimalnej wartoœci wêz³a z listy korzeni (wskaŸnik brata ustawiany jest na nullptr, poniewa¿ usuwany korzeñ jest ostatnim na liœcie korzeni)
+		else
+			tmp->setSibling(tmp->getSibling()->getSibling()); //usuniêcie korzenia o minimalnej wartoœci wêz³a z listy korzeni( sytuacja, w której wêze³ usuwany nie jest ostatnim korzeniem na liœcie korzeni)
+	}
+	//else {
+		
+	//}
 
 
 	Binomial_Heap* newHeap = make_binomial_heap();
 
+	if (minCh != nullptr) {			//jeœli usuwany wêze³ nie jest wêz³em o stopniu zerowym
 
+		while (minCh != nullptr) {		//dodanie listy dzieci korzenia minimalnego do wektora
+			tabN.push_back(minCh);
+			minCh->setParent(nullptr); //zerwanie po³¹czenia z minimalnym wêz³em
+			minCh = minCh->getSibling();
+		}
 
-	while (minCh != nullptr) {		//dodanie listy dzieci korzenia minimalnego do wektora
-		tabN.push_back(minCh);
-		minCh->setParent(nullptr); //zerwanie po³¹czenia z minimalnym wêz³em
-		minCh = minCh->getSibling();
+		/*for (int i = 0; i < tabN.size(); i++)
+		{
+			std::cout << tabN[i]->getValue() << " ";
+		}
+		std::cout << std::endl;*/
+
+		for (int i = tabN.size() - 1; i > 0; i--) {		//odwrócenie kolejnoœcie elementów na liœcie synów wêz³a minimalnego
+
+			tabN[i]->setSibling(tabN[i - 1]);
+		}
+		tabN[0]->setSibling(nullptr); //w przyk³adzie z naszej prezentacji jest to wêze³ o wartoœci 4 i ustawiamy tak wskaŸnik ¿eby nie mia³ brata po prawej stronie
+
+		tmp = tabN[tabN.size() - 1];
 	}
-
-	/*for (int i = 0; i < tabN.size(); i++)
-	{
-		std::cout << tabN[i]->getValue() << " ";
+	else {	//w przeciwnym wypadku 
+		tmp = nullptr;
 	}
-	std::cout << std::endl;*/
-
-	for (int i = tabN.size() - 1; i > 0; i--) {		//odwrócenie kolejnoœcie elementów na liœcie synów wêz³a minimalnego
-
-		tabN[i]->setSibling(tabN[i - 1]);
-	}
-
-	tabN[0]->setSibling(nullptr); //w przyk³adzie z naszej prezentacji jest to wêze³ o wartoœci 4 i ustawiamy tak wskaŸnik ¿eby nie mia³ brata po prawej stronie
-
-	tmp = tabN[tabN.size()-1];
 	newHeap->setRoot(tmp);
 	newHeap->setSize(pow(2, min->getDegree()) - 1);
 
 	Binomial_Heap* heap = new Binomial_Heap();
 	if (min != H->getRoot())
 		heap = H;
+	else if (secondRoot != nullptr)				//jeœli g³owa jest najmniejszym wêz³em							
+		heap->setRoot(secondRoot);
 	else							//jeœli kopiec dwumianowy sk³ada siê z jednego drzewa dwumianowego
 		heap->setRoot(nullptr);
 
--- alg_KD/Source.cpp
+++ alg_KD/Source.cpp
@@ -19,12 +19,12 @@ int main()
 	/*Budowanie kopca
 	15			->		4			->			1
 	|				/	|				/	/	|
-	18				15	8				4	6	5	
+	18				15	8				4	6	5
 					|				/	|	|	|
-					20				15	8	9	
-									|	
-									20	
-	*/								
+					20				15	8	9
+									|
+									20
+	*/
 
 	//INSERT TEST
 	H1 = binomial_heap_insert(H1, 1);
@@ -50,9 +50,10 @@ int main()
 
 
 	//DECREASE KEY TEST
-
+	cout << endl << "------------------ DECREASE KEY TEST ------------------" << endl;
 	//wg moich obliczeñ to powinien byæ wêze³ z wartoœci¹ 20 
 	Node* decrease_test = H1->getRoot()->getSibling()->getSibling()->getChild()->getChild()->getChild();
+	cout << "Zamiana wartosci wezle (znajdujacym sie w drzewie dwumianowym ktorego korzen jest ostatnim na liscie korzeni) z 20 na 2";
 	binomial_heap_decrease_key(H1, decrease_test, 2);
 	cout << "\nKopiec H1 po zmianie wartoœci klucza:\n";
 	H1->print(H1->getRoot());
@@ -60,13 +61,13 @@ int main()
 
 	//trzeci korzeñ na liœcie, z wartoœci¹ 1
 	decrease_test = H1->getRoot()->getSibling()->getSibling();
-	cout << "\nKopiec H1 próba zmiany klucza na wiêkszy:\n";
+	cout << "\nKopiec H1 próba zmiany klucza na wiêkszy (z 1 na 5):\n";
 	binomial_heap_decrease_key(H1, decrease_test, 5);
 	H1->print(H1->getRoot());
 
 	//nullptr przekazany do funkcji
 	decrease_test = nullptr;
-	cout << "\nKopiec H1 próba zmiany klucza na wiêkszy:\n";
+	cout << "\nKopiec H1 próba zmiany klucza na wiêkszy(przekazywany wskaznik wskazuje na nullptr):\n";
 	binomial_heap_decrease_key(H1, decrease_test, 5);
 	H1->print(H1->getRoot());
 
@@ -76,26 +77,31 @@ int main()
 
 	//EXTRACT MIN TEST
 	int min;
-
+	cout <<endl << "------------------ EXTRACT MIN TEST ------------------" << endl;
 	//usuwanie z ostatniego drzewa
+	cout << "Usuwanie wezla posiadajacego dzieci oraz bedacego korzeniem ostatnim na liscie korzeni.";
 	min = binomial_heap_extract_min(H1)->getValue();
 	cout << "\nUsuniêta wartoœæ: " << min;
 	cout << "\nKopiec H1: " << endl;
 	H1->print(H1->getRoot());
 
 	//usuwanie z ostatniego drzewa
-	min = binomial_heap_extract_min(H1)->getValue();
-	cout<<"\nUsuniêta wartoœæ: "<<min;
-	cout << "\nKopiec H1: " << endl;
-	H1->print(H1->getRoot());
-
-	//usuwanie z pierwszego drzewa
+	cout << endl << "Usuwanie wezla posiadajacego dzieci oraz bedacego korzeniem ostatnim na liscie korzeni.";
 	min = binomial_heap_extract_min(H1)->getValue();
 	cout << "\nUsuniêta wartoœæ: " << min;
 	cout << "\nKopiec H1: " << endl;
 	H1->print(H1->getRoot());
 
-	cout << "\nKopiec H1 - usuwanie korzenia, ze œrodkowego drzewa: " << endl;
+	//usuwanie z ostatniego drzewa
+	cout << endl << "Usuwanie wezla posiadajacego dzieci oraz bedacego korzeniem pierwszym na liscie korzeni.";
+	min = binomial_heap_extract_min(H1)->getValue();
+	cout << "\nUsuniêta wartoœæ: " << min;
+	cout << "\nKopiec H1: " << endl;
+	H1->print(H1->getRoot());
+
+	//cout << "\nKopiec H1 - usuwanie korzenia, ze œrodkowego drzewa: " << endl;
+	cout << endl << "Usuwanie wezla posiadajacego dzieci oraz bedacego korzeniem drugim (œrodkowym) na liscie korzeni." << endl;
+	cout << "Kopiec H1 po dodaniu wezla o kluczu 1 oraz wezla o kluczu 5: " << endl;
 	H1 = binomial_heap_insert(H1, 1);
 	H1 = binomial_heap_insert(H1, 5);
 	H1->print(H1->getRoot());
@@ -107,11 +113,24 @@ int main()
 	H1->print(H1->getRoot());
 
 	//usuwanie z ostatniego drzewa
+	cout << endl << "Usuwanie wezla posiadajacego dzieci oraz bedacego korzeniem drugim (ostatnim) na liscie korzeni.";
 	min = binomial_heap_extract_min(H1)->getValue();
 	cout << "\nUsuniêta wartoœæ: " << min;
 	cout << "\nKopiec H1: " << endl;
 	H1->print(H1->getRoot());
 
+	//cout << "\nKopiec H1 - usuwanie korzenia, z pierwszego drzewa: " << endl;
+	cout << endl << "Usuwanie wezla nieposiadajacego dzieci oraz bedacego korzeniem pierwszym na liscie korzeni." << endl;
+	cout << "Kopiec H1 po dodaniu wezla o kluczu 3 oraz wezla o kluczu 1: " << endl;
+	H1 = binomial_heap_insert(H1, 3);
+	H1 = binomial_heap_insert(H1, 1);
+	H1->print(H1->getRoot());
+
+	//usuwanie z pierwszego drzewa
+	min = binomial_heap_extract_min(H1)->getValue();
+	cout << "\nUsuniêta wartoœæ: " << min;
+	cout << "\nKopiec H1: " << endl;
+	H1->print(H1->getRoot());
 
 	Binomial_Heap* H2 = make_binomial_heap();
 
@@ -132,10 +151,11 @@ int main()
 	cout << "Kopiec H2: " << endl;
 
 	H2->print(H2->getRoot());
-
+	cout << endl << "------------------ DELETE TEST ------------------" << endl;
 	Node* delete_test = H2->getRoot()->getChild(); //wskaŸnik na wêze³ przechowuj¹cy wartoœc 4 tak jak w naszym pdfie
-
-	binomial_heap_delete(H2,delete_test);
-	cout << endl;
+	
+	cout << "Usuniecie wezla o wartosci 4 z kopca." << endl;
+	cout << "Kopiec H2:" << endl;
+	binomial_heap_delete(H2, delete_test);
 	H2->print(H2->getRoot());
 }
